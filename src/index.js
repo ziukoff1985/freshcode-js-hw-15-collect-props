58afa1043e8f518abb4ba2b182972ae95d0d876c
Borys Ziukov <ziukoff1985@gmail.com>
2025-10-07 10:14:51 +0300

    Fix form appearance

--- src/index.js
+++ src/index.js
@@ -1,44 +1,173 @@
 'use strict';
 
-const form = document.querySelector('form');
+import { inputConfigData, radioConfigData } from './configData.js';
 
-// Final version
+// DOM - elements creation
+const container = document.createElement('div');
+container.classList.add('container');
+document.body.prepend(container);
+
+const form = document.createElement('form');
+container.append(form);
+
+const h1 = document.createElement('h1');
+h1.textContent = 'CREATE AN ACCOUNT';
+form.append(h1);
+
+const inputWrapDiv = document.createElement('div');
+inputWrapDiv.classList.add('inputs-wrapper');
+form.append(inputWrapDiv);
+
+inputConfigData.forEach(({ type, name, placeholder, attributes }) => {
+    const input = document.createElement('input');
+    input.setAttribute('type', type);
+    input.setAttribute('name', name);
+    input.setAttribute('placeholder', placeholder);
+    input.setAttribute('aria-label', placeholder);
+
+    if (attributes) {
+        for (const key in attributes) {
+            input.setAttribute(key, attributes[key]);
+        }
+    }
+
+    inputWrapDiv.append(input);
+});
+
+radioConfigData.forEach(({ id, label, paragraphText }) => {
+    const radioWrapDiv = document.createElement('div');
+    radioWrapDiv.classList.add('radio-wrapper');
+    form.append(radioWrapDiv);
+
+    const radioInput = document.createElement('input');
+    radioInput.setAttribute('type', 'radio');
+    radioInput.setAttribute('name', 'join-as');
+    radioInput.setAttribute('id', id);
+    radioWrapDiv.append(radioInput);
+
+    const radioContentDiv = document.createElement('div');
+    radioContentDiv.classList.add('radio-content');
+    radioWrapDiv.append(radioContentDiv);
+
+    const radioContentLabel = document.createElement('label');
+    radioContentLabel.setAttribute('for', id);
+    radioContentLabel.textContent = label;
+    radioContentDiv.append(radioContentLabel);
+
+    const radioContentParagraph = document.createElement('p');
+    radioContentParagraph.classList.add('radio-text');
+    radioContentParagraph.textContent = paragraphText;
+    radioContentDiv.append(radioContentParagraph);
+});
+
+const checkboxWrapDiv = document.createElement('div');
+checkboxWrapDiv.classList.add('checkbox-wrapper');
+form.append(checkboxWrapDiv);
+
+const checkboxInput = document.createElement('input');
+checkboxInput.setAttribute('type', 'checkbox');
+checkboxInput.setAttribute('name', 'terms');
+checkboxInput.setAttribute('id', 'terms');
+checkboxWrapDiv.append(checkboxInput);
+
+const checkboxInputLabel = document.createElement('label');
+checkboxInputLabel.setAttribute('for', 'terms');
+checkboxInputLabel.textContent =
+    'Allow Squadhelp to send marketing/promotional offers from time to time';
+checkboxWrapDiv.append(checkboxInputLabel);
+
+const btnWrapDiv = document.createElement('div');
+btnWrapDiv.classList.add('btn-wrapper');
+form.append(btnWrapDiv);
+
+const submitButton = document.createElement('button');
+submitButton.setAttribute('type', 'submit');
+submitButton.textContent = 'Submit';
+btnWrapDiv.append(submitButton);
+
+const cancelButton = document.createElement('button');
+cancelButton.setAttribute('type', 'reset');
+cancelButton.textContent = 'Cancel';
+btnWrapDiv.append(cancelButton);
+
+// DOM - events creation
+
 class Person {
-    constructor(
-        firstName = 'Not provided',
-        lastName,
-        nickName = 'Not provided',
-        email = 'Not provided'
-    ) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.nickName = nickName;
-        this.email = email;
+    constructor(...args) {
+        args.forEach(({ name, value }) => {
+            this[name] = value.trim();
+        });
     }
 }
 
 function onSubmitForm(event) {
     event.preventDefault();
-    const formElements = form.elements;
+    const formInputs = [...document.querySelectorAll('input')].filter(
+        ({ name, value, type }) =>
+            name && value.trim() && type !== 'checkbox' && type !== 'radio'
+    );
 
-    const person = new Person();
-
-    for (const { name, value, tagName } of formElements) {
-        if (name && tagName === 'INPUT' && value.trim()) {
-            person[name] = value.trim();
-        }
-    }
+    const person = new Person(...formInputs);
 
     if (!person.lastName) {
         console.log('Cannot save: Last Name is required and cannot be empty');
         return;
     }
 
-    localStorage.setItem(person.lastName, JSON.stringify(person));
+    const personJson = JSON.stringify(
+        person,
+        (key, value) =>
+            key === 'password' || key === 'passwordConfirmation'
+                ? undefined
+                : value,
+        2
+    );
+
+    localStorage.setItem(person.lastName, personJson);
     form.reset();
 }
 
 form.addEventListener('submit', onSubmitForm);
+
+// const form = document.querySelector('form');
+
+// * Version with dynamic props collection
+// class Person {
+//     constructor(
+//         firstName = 'Not provided',
+//         lastName,
+//         nickName = 'Not provided',
+//         email = 'Not provided'
+//     ) {
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.nickName = nickName;
+//         this.email = email;
+//     }
+// }
+
+// function onSubmitForm(event) {
+//     event.preventDefault();
+//     const formElements = form.elements;
+
+//     const person = new Person();
+
+//     for (const { name, value, tagName } of formElements) {
+//         if (name && tagName === 'INPUT' && value.trim()) {
+//             person[name] = value.trim();
+//         }
+//     }
+
+//     if (!person.lastName) {
+//         console.log('Cannot save: Last Name is required and cannot be empty');
+//         return;
+//     }
+
+//     localStorage.setItem(person.lastName, JSON.stringify(person));
+//     form.reset();
+// }
+
+// form.addEventListener('submit', onSubmitForm);
 
 // * Version with event.target.elements
 
